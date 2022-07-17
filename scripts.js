4584eca5ea35a1b66c3b4412a43e132a29db5bc5
zelwake <michalhladik2@seznam.cz>
2022-07-17 17:21:53 +0200

    v1

--- scripts.js
+++ scripts.js
@@ -6,6 +6,9 @@ $("#secondButton").click(() => {
     $("#firstButton").text("Clicked");
 })
 
+let colorInput = $("#colorInInput").val();
+console.log($("#colorInInput").val())
+
 $("#thirdButton").click(() => {
-    $("button").css("background-color","red");
+    $("button").css("background-color", colorInput);
 })
