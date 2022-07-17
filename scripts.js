f498492c78d603cc26f60d30f23267e795236ed1
zelwake <michalhladik2@seznam.cz>
2022-07-17 17:28:31 +0200

    v2

--- scripts.js
+++ scripts.js
@@ -6,7 +6,11 @@ $("#secondButton").click(() => {
     $("#firstButton").text("Clicked");
 })
 
+//  read color in input once a never update
+//  let colorInput = $("#colorInInput").val();
+
 $("#thirdButton").click(() => {
+//  update variable with each click based on input
     let colorInput = $("#colorInInput").val();
     $("button").css("background-color", colorInput);
 })
